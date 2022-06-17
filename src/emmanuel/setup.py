91f804b5b8905d8577176016c38ebf848f29e885
Geniools <gumaniuc.alexandru@mail.ru>
2022-06-17 15:17:22 +0200

    SMTH EVEN MORE

--- src/emmanuel/setup.py
+++ src/emmanuel/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'motor_run = emmanuel.motor_run:main',
+            'motor_run = emmanuel.motors:main',
         ],
     },
 )
