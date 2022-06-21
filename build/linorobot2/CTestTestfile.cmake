aeda6f73e43df10978e58757cd2ff1ea994c08e9
Geniools <gumaniuc.alexandru@mail.ru>
2022-06-21 15:22:05 +0200

    Deleted everything

--- build/linorobot2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/Emmanuel_Jr_robot/src/linorobot2/linorobot2
-# Build directory: /home/ubuntu/Emmanuel_Jr_robot/build/linorobot2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/mpu9250driver/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/Emmanuel_Jr_robot/src/ros2_mpu9250_driver
-# Build directory: /home/ubuntu/Emmanuel_Jr_robot/build/mpu9250driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
